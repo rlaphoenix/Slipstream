b0dc45415ac256f28f000d6fe7b770c59df8cc99
PHOENiX <rlaphoenix@pm.me>
2021-04-11 09:50:39 +0100

    ui: Flag UI as Window, support passing parent

--- pslipstream/ui/main.py
+++ pslipstream/ui/main.py
@@ -35,12 +35,13 @@ class Devices(QtCore.QObject):
 
 class UI(QMainWindow):
 
-    def __init__(self):
-        super(UI, self).__init__()
+    def __init__(self, parent=None):
+        super(UI, self).__init__(parent)
         ui_loader = QUiLoader()
         ui_file = QtCore.QFile(os.path.join(os.path.dirname(__file__), "form.ui"))
         ui_file.open(QtCore.QFile.ReadOnly)
-        self.widget = ui_loader.load(ui_file, self)
+        self.widget = ui_loader.load(ui_file, parent)
+        self.widget.setWindowFlags(QtCore.Qt.Window)
         ui_file.close()
 
         self.devices = []
