a63dc9f7bfafd2a24e3d91446a588d83b6490c5f
PHOENiX <rlaphoenix@pm.me>
2021-04-12 07:32:31 +0100

    Clean up setup_commands init

--- setup_commands/__init__.py
+++ setup_commands/__init__.py
@@ -10,15 +10,18 @@ def print_bold(s):
     print("\033[1m{0}\033[0m".format(s))
 
 
-def build_clean():
-    print_bold("Removing previous builds…")
+def clean():
+    """Clean build data directories."""
     rmtree(cfg.root_dir / "dist", ignore_errors=True)
     rmtree(cfg.root_dir / "build", ignore_errors=True)
 
 
-def build():
-    build_clean()
+def build(clean_prior=True):
+    """Build source and universal wheel distributions."""
+    if clean_prior:
+        print_bold("Removing previous builds…")
+        clean()
     print_bold("Ensuring an up-to-date environment…")
-    os.system("{0} -m pip install --user --upgrade setuptools wheel".format(sys.executable))
+    os.system(f"{sys.executable} -m pip install --user --upgrade setuptools wheel")
     print_bold("Building Source and Wheel (universal) distribution…")
-    os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+    os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
