df0b5df8cbf9b0d29b4d380dac657d6206486743
PHOENiX <rlaphoenix@pm.me>
2021-04-17 21:57:42 +0100

    pyinstaller: Remove /build and spec if not debug

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,9 +1,11 @@
 import itertools
 import os
+import shutil
 
 from PyInstaller.__main__ import run
 
 """Configuration options that may be changed or referenced often."""
+DEBUG = False  # When False, removes un-needed data after build has finished
 NAME = "Slipstream"
 ICON_FILE = "pslipstream/static/img/icon.ico"  # pass None to use default icon
 ONE_FILE = False  # Must be False if using setup.iss
@@ -30,3 +32,7 @@ run([
     *itertools.chain(*[["--hidden-import", x] for x in HIDDEN_IMPORTS]),
     *EXTRA_ARGS
 ])
+
+if not DEBUG:
+    shutil.rmtree("build")
+    os.unlink("Slipstream.spec")
