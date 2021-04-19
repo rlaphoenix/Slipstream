49d9a92bb7597e55219501e6ccd7290be222b010
PHOENiX <rlaphoenix@pm.me>
2021-04-19 04:40:49 +0100

    Device: Add more options, is_file, & make model fallback

--- pslipstream/device.py
+++ pslipstream/device.py
@@ -1,7 +1,11 @@
 class Device:
-    def __init__(self, target: str, make: str = None, model: str = None, revision: str = None, volume_id: str = None):
+    def __init__(self, target: str, medium: str = None, make: str = None, model: str = None, revision: str = None,
+                 volume_id: str = None):
         self.target = target
-        self.make = make
-        self.model = model
+        self.medium = medium
+        self.make = make or "Virtual"
+        self.model = model or "FS"
         self.revision = revision
         self.volume_id = volume_id
+
+        self.is_file = target.lower().endswith(".iso") or target.lower().endswith(".ifo")
